865178b3a58ec5942e64745c107e9c97b3280369
Ramkishan <er.rkjangir9602@gmail.com>
2025-08-15 13:30:43 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from src.logger import logging
 
 def error_message_details(error, error_details: sys):
     _, _, exc_tb = error_details.exc_info()
@@ -16,7 +17,6 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-
 
       
   
